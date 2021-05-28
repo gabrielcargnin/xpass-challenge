823cb2d23eb937922bfe47920a77cb0001a0168b
Gabriel Aguiar Cargnin <gabrielcargnin@gmail.com>
2021-05-28 03:49:31 -0300

    Fixing GCP configurations.

--- src/main/java/com/github/gabrielcargnin/xpasschallenge/Application.java
+++ src/main/java/com/github/gabrielcargnin/xpasschallenge/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class XpassChallengeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XpassChallengeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
